19b63a4c6da398c3c130e069e18ae540b911d17a
zsun114 <115062701+zsun114@users.noreply.github.com>
2024-03-18 14:27:17 -0700

    Add files via upload

--- KIA_Brake.py
+++ KIA_Brake.py
@@ -1,47 +1,26 @@
 import can
 import cantools
-import time
+from opendbc.can.packer import CANPacker
+from openpilot.selfdrive.car.hyundai import hyundaican
+from openpilot.selfdrive.car.hyundai.values import CAR
 from inputs import get_gamepad
-from pprint import pprint
-db = cantools.database.load_file('directory of dbc file')#need to chage your own directory where you put dbc file
+from panda import Panda
+db = cantools.database.load_file('hyundai_kia_generic.dbc')#need to chage your own directory where you put dbc file
 # Initialize the CANbus port
 bus = can.interface.Bus(channel = 'can0', bustype= 'socketcan_native')
 
-# the ID of CAN mesage through the Braking function
+#Init panda
+p = Panda()
 
-BRAKE_ID = None
-for message in bus:
-    if message.arbitration_id == 881:  ##Braking Pedal position message ID
-        BRAKE_ID = message.arbitration_id
-        break
-
-# Reading Braking Pedal message
-def read_beake_data()
-    time_start = time.time()
-    try:
-        while True:
-            message = bus.recv()
-            if message is not None:
-                try:
-                    #Decoding CAN message
-                    message_decoded = db.decode_message(message.arbitration_id, message.data)
-                    if message.arbitration_id == BRAKE_ID
-                        brake_message = message.data[0] 
-                        pprint("Brake message:", brake_message)
-                        time_duration = time.time() - time_start
-                        dataList.append({'Duration': time_duration, 'data': brake_message})
-                except Exception as e:
-                    a = 0
-    except:
-        pass
-
+# Set safety mode to allow everything
+p.set_safety_mode(p.SAFETY_ALLOUTPUT)
 
 def update_brake_pressure():
     global brake_pressure
     while True:
         events = get_gamepad()
         for event in events:
-            if event.ev_type == "Absolute" and event.code == "ABS_Z"
+            if event.ev_type == "Absolute" and event.code == "ABS_Z":
                 brake_pressure = int(event.state / 2.55) 
                 brake_message = can.Message(arbitration_id=BRAKE_ID, data=[brake_pressure])
                 # 发送刹车控制消息
